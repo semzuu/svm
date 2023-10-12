947b1d2fe032a5d963fb290f0b30284f6141ba67
semzuu <mohamedkhm28+github@gmail.com>
2023-10-12 22:02:45 +0100

    Added Reading/Writing instructions from/to files

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-gcc svm.c -o svm
-
+gcc svm.c -o svm 
+#gcc file_test.c -o file_test
 
--- svm.c
+++ svm.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
+#include <stdint.h>
 
 #define MAX_STACK_SIZE 1024
-#define PROG_SIZE (sizeof(program)/sizeof(program[0]))
+#define PROG_SIZE (sizeof(prog)/sizeof(INST))
 
 typedef enum {
     INST_PUSH,
@@ -17,12 +18,13 @@ typedef enum {
 typedef enum {
     ERR_DIV_BY_ZERO,
     ERR_STACK_EMPTY,
-    ERR_STACK_FULL
+    ERR_STACK_FULL,
+    ERR_FILE_NOT_FOUND
 } ERR;
 
 typedef struct {
     INST_TYPE type;
-    int value;
+    uint16_t value;
 }INST;
 
 #define PUSH(x) {.type=INST_PUSH, .value=(x)}
@@ -37,39 +39,38 @@ typedef struct {
     int* stack;
 } VM;
 
-/*
 INST* load_file(char* filename){
-    FILE* f;
-    f = fopen(filename, "rb");
-    if (f == NULL) { err(ERR_FILE_NOT_FOUND); exit(1);}
-    fseek(fp, 0, SEEK_END);
+    FILE* f = fopen(filename, "rb");
+    if (f == NULL) { 
+        err(ERR_FILE_NOT_FOUND); 
+        exit(1);
+    }
+    fseek(f, 0, SEEK_END);
     int file_size = ftell(f);
+    rewind(f);
     int n = file_size/sizeof(INST);
     INST* prog = (INST*) malloc(sizeof(INST)*n);
-    for (int i=0; i< n; i++){
-        fread( &(prog[i]), sizeof(INST), 1, f);
-    }
+    fread( prog, sizeof(INST), n, f);
+    fclose(f);
+    return prog;
 }
 
-void write_file(char* filename){
-    FILE* f;
-    f = fopen(filename, "wb");
-    if (f == NULL) { exit(1);}
-
+void write_file(char* filename, INST* prog, int length){
+    FILE* f = fopen(filename, "wb");
+    fwrite(prog, sizeof(INST), length, f);
+    fclose(f);
+    return ;
 }
-*/
 
-INST program[] = {
+
+INST prog[] = {
+    PUSH(10),
     PUSH(10),
     PUSH(5),
+    POP(),
     ADD(),
-    PUSH(8),
-    SUB(),
     PUSH(2),
-    MUL(),
-    PUSH(2),
-    DIV(),
-    POP()
+    MUL()
 };
 
 
@@ -88,6 +89,7 @@ char* err_to_string(ERR type){
         case ERR_DIV_BY_ZERO: return "ERR_DIV_BY_ZERO";
         case ERR_STACK_EMPTY: return "ERR_STACK_EMPTY";
         case ERR_STACK_FULL: return "ERR_STACK_FULL";
+        case ERR_FILE_NOT_FOUND: return "ERR_FILE_NOT_FOUND";
     }
 }
 
@@ -95,12 +97,16 @@ char* err_to_string(ERR type){
 
 int main(){
     VM v = {.stack_size=0, .stack=(int*) malloc(sizeof(int)*MAX_STACK_SIZE) };
-    for(int i=0; i<PROG_SIZE; i++){
+
+    write_file("test.svm", prog, PROG_SIZE);
+    INST* program = load_file("test.svm");
+
+    for(int ip=0; ip<PROG_SIZE; ip++){
         int a,b;
-        switch(program[i].type){
+        switch(program[ip].type){
             case INST_PUSH:
                 if ( v.stack_size == MAX_STACK_SIZE ) { err(ERR_STACK_FULL); exit(1);}
-                push(&v, program[i].value);
+                push(&v, program[ip].value);
                 break;
             case INST_POP:
                 if ( v.stack_size == 0 ) { err(ERR_STACK_EMPTY); exit(1);}
@@ -133,7 +139,7 @@ int main(){
                 break;
         }
 
-        for(int i=0; i<v.stack_size; i++) {printf("%d--%d\n",i,v.stack[i]);}
+        for(int i=v.stack_size-1; i>=0; i--) {printf("%d--%d\n",i,v.stack[i]);}
         printf("\n");
     }
     return 0;
